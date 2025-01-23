687f621bfacf3cf2d3e19e0b3133dcf8f53bd825
Anatole <anatole.giniaux@sfr.fr>
2025-01-23 13:51:50 +0100

    feat: fps counter

---
+++ Assets/Game/Scripts/UI/FPSCounter.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI fpsText;
+
+    private int _frameCount;
+    private float _deltaTime;
+    private const int FrameRange = 100;
+
+    void Update()
+    {
+        _frameCount++;
+        _deltaTime += Time.smoothDeltaTime;
+
+        if (_frameCount >= FrameRange)
+        {
+            float averageFPS = FrameRange / _deltaTime;
+            fpsText.text = $"{averageFPS:0} FPS";
+            _frameCount = 0;
+            _deltaTime = 0.0f;
+        }
+    }
+}
