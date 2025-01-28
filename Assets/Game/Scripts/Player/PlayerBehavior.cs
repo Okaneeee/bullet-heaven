fbfdcedd09222b13a48de04d121182df4449d7f2
Anatole <anatole.giniaux@sfr.fr>
2025-01-28 17:52:56 +0100

    feat: bars on ui

--- Assets/Game/Scripts/Player/PlayerBehavior.cs
+++ Assets/Game/Scripts/Player/PlayerBehavior.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class PlayerBehavior : MonoBehaviour
 {
     public static PlayerBehavior Instance { get; private set; }
     
+    [FormerlySerializedAs("health")]
     [Header("Stats")]
     [SerializeField]
-    private float health = 100.0f;
+    private float maxHealth = 100.0f;
+    private float _currentHealth;
     [SerializeField]
     private float damage = 34.0f;
+    
+    [SerializeField]
+    private Slider healthBar;
     
     void Awake()
     {
@@ -22,11 +29,20 @@ public class PlayerBehavior : MonoBehaviour
         }
     }
     
+    void Start()
+    {
+        healthBar.maxValue = maxHealth;
+        healthBar.value = maxHealth;
+        
+        _currentHealth = maxHealth;
+    }
+    
     public void TakeDamage(float dmg)
     {
-        health -= dmg;
+        _currentHealth -= dmg;
+        healthBar.value = _currentHealth;
         
-        if (health <= 0)
+        if (_currentHealth <= 0)
         {
             Die();
         }
