fcb8066fc361a1d944a2543f53c44c7048fe9f64
Anatole <anatole.giniaux@sfr.fr>
2025-01-28 16:22:41 +0100

    feat: game over screen display time & score

--- Assets/Game/Scripts/UI/Menu/GameOverMenu.cs
+++ Assets/Game/Scripts/UI/Menu/GameOverMenu.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,6 +9,12 @@ public class GameOverMenu : MonoBehaviour
     private string gameScene;
     [SerializeField]
     private string mainMenuScene;
+    
+    [Header("Text")]
+    [SerializeField]
+    private TextMeshProUGUI scoreText;
+    [SerializeField]
+    private TextMeshProUGUI timeText;
     
     public void Replay()
     {
@@ -23,5 +30,11 @@ public class GameOverMenu : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+    
+    void Start()
+    {
+        scoreText.text = $"Score: {GameManager.Instance.GetScore():00000}";
+        timeText.text = $"Time alive:<br>{GameManager.Instance.GetTime() / 60:00}:{GameManager.Instance.GetTime() % 60:00}";
     }
 }
